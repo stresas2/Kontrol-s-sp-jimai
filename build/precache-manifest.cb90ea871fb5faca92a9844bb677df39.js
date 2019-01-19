e318b673ccb158ae980378330d792e2740c97546
stresas2 <44831849+stresas2@users.noreply.github.com>
2019-01-19 12:16:17 +0200

    Add files via upload

---
+++ build/precache-manifest.cb90ea871fb5faca92a9844bb677df39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a1aa4c1c330401e370c5",
+    "url": "/static/js/main.a1aa4c1c.chunk.js"
+  },
+  {
+    "revision": "961f6db60ef1cc7be5d5",
+    "url": "/static/js/1.961f6db6.chunk.js"
+  },
+  {
+    "revision": "a1aa4c1c330401e370c5",
+    "url": "/static/css/main.e29c0ea4.chunk.css"
+  },
+  {
+    "revision": "961f6db60ef1cc7be5d5",
+    "url": "/static/css/1.a78d2d27.chunk.css"
+  },
+  {
+    "revision": "c05c9d28ac222f2d0753e3c612b03e13",
+    "url": "/index.html"
+  }
+];
